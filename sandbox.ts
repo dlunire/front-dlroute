1f70e1d33a409d647fe60fc6fdab1f475ace48e8
David Eduardo Luna Montilla <dlunireframework@gmail.com>
2026-07-02 03:31:22 -0500

    Update

--- sandbox.ts
+++ sandbox.ts
@@ -1,2 +1,9 @@
 import * as routing from "./index";
 
+routing.route("/ciencia", function () {
+    console.log({ test: true, message: "Funciona" });
+});
+
+const component: Function | null = routing.dispatch().component as typeof Function | null;
+
+console.log({ component });
